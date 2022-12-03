eca7f967c3ec40b00bb048dba74bb7eb490643a3
UdaySinghArya <118660131+UdaySinghArya@users.noreply.github.com>
2022-12-03 23:53:56 +0530

    Add files via upload

---
+++ assignment 8 (2).py
@@ -0,0 +1,83 @@
+"""
+#PROGRAM-1-write a python script to print MySirG five times on the screen
+i=1
+while i<=5:
+    print("MYSirG")
+    i+=1
+
+"""
+"""
+#PROGRAM-2-write a python script to print first 10 natural number
+i=1
+while i<=10:
+    print(i)
+    i+=1
+"""
+"""
+#PROGRAM-3-write a python script to print first 10 natural number in reverse order
+i=10
+while i>0:
+    print(i)
+    i-=1
+"""
+"""
+#PROGRAM-4-write a python script to print first 10 odd natural number
+i=1
+while i<=20:
+    print(i)
+    i+=2
+"""
+"""
+#PROGRAM-5-write a python script to print first 10 odd natural number in reverse order
+i=19
+while i>0:
+    print(i)
+    i-=2
+"""
+"""
+#PROGRAM-6-write a python script to print first 10 even natural number
+i=2
+while i<=20:
+    print(i)
+    i+=2
+"""
+"""
+#PROGRAM-7-write a python script to print first 10 even natural number in reverse order
+i=20
+while i>0:
+    print(i)
+    i-=2
+"""
+"""
+#PROGRAM-8-write a python script to print square of first 10  natural number 
+i=1
+while i<=10:
+    j=i*i
+    print(j)
+    i+=1
+
+"""
+"""
+#PROGRAM-9-write a python script to print cube of first 10  natural number
+i=1
+while i<=10:
+    j=i*i*i
+    print(j)
+    i+=1
+"""
+"""
+#PROGRAM-10-write a python script to print cube of first 10  natural number
+i=1
+while i<=10:
+    j=5*i
+    print(j)
+    i+=1
+"""
+
+
+
+
+
+
+
+
