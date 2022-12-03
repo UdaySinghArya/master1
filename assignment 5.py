4b70201892af7c783c1f8bf0bd220c3b5f270879
UdaySinghArya <118660131+UdaySinghArya@users.noreply.github.com>
2022-12-03 23:49:57 +0530

    Add files via upload

---
+++ assignment 5.py
@@ -0,0 +1,59 @@
+
+
+#PROGRAM-1-write a python script To remove last digit from the given number.
+
+x=2534
+y=x//10  #it is a floor division method
+print(y)
+
+
+#PROGRAM-1-write a python script To get last digit from the given number.
+"""
+x=2089214445693
+y=x%10
+print(y)
+"""
+
+#PROGRAM-3-write a python script to find x power y are given by the user
+"""
+x=int(input("value of x is= "))
+y=int(input("value of y is= "))
+z=x**y
+print("power of x is = ",y)
+print("value  of x is = ",z)
+"""
+
+#PROGRAM-4-write a python script to swap data of two int variables
+"""
+x=int(input("value of x is= "))
+y=int(input("value of y is= "))
+print("\n")
+x,y=y,x
+print(x,y)
+"""
+
+#PROGRAM-5-write a python script to take three digit number from the user and display only its middle digit
+
+x=int(input(print("enter a number= "))
+y=x//10
+z=y%10
+print("middle digit this number is= ",z)
+
+
+#PROGRAM-5-write a python script to take three digit number from the user and display only its first digit
+"""
+x=int(input("enter a number= "))
+y=x//10
+z=y//10
+print("first digit this number is= ",z)
+
+"""
+
+
+
+
+
+
+
+
+
