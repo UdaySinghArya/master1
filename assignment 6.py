24841f53d6be2e22e62bbc7bcfc4057424d57d77
UdaySinghArya <118660131+UdaySinghArya@users.noreply.github.com>
2022-12-03 23:51:29 +0530

    Add files via upload

---
+++ assignment 6.py
@@ -0,0 +1,71 @@
+
+
+#PROGRAM-1-To check whether a given number is positive or negative
+"""
+x=int(input("enter a number= "))
+if x>0:
+    print("number is positive")
+else:
+    print("non  positive number")
+
+"""
+#PROGRAM-2-To check whether a given number is divisible by 5 or not
+"""
+x=int(input("enter a number= "))
+if x%5==0:
+    print("divisible by 5")
+else:
+    print("not divisible by 5")
+
+"""
+#PROGRAM-3-To check whether a given number is even or odd
+"""
+x=int(input("enter a number= "))
+if x%2==0:
+    print(" number is even ")
+else:
+    print(" number is odd ")
+
+"""
+
+#PROGRAM-4-To print greater between two numbers.print number only once if both the numbers are same
+"""
+x=int(input("enter first number= "))
+y=int(input("enter second number= "))
+if x>y:
+    print("greater is ",x)
+elif x==y:
+    print("both the numbers are same",x)
+else:
+    print("greater is",y)
+
+"""
+
+
+#PROGRAM-5-write a python script to print two given word in dictonary order
+"""
+x=input('ENTER  FIRST STRING= ' )
+y=input('ENTER  SECOND STRING= ')
+if x<y:
+    print(x,y)
+else:
+    print(y,x)
+
+"""
+
+#PROGRAM-5-write a python script to check whether a given number is a three digit or not
+"""
+x=int(input("ENTER A NUMBER"))
+
+if (x>99) and (x<=1000):
+    print(x," is 3 digit number")
+else:
+    print(x," is not 3 digit number")
+
+"""
+
+    
+
+
+
+
