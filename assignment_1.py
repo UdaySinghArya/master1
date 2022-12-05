f6827cc09b5dd181cc93b60ddbf9d79c7eb193b0
UdaySinghArya <118660131+UdaySinghArya@users.noreply.github.com>
2022-12-06 00:02:54 +0530

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,43 @@
+#Program-4-
+'''
+print("Hello Python")
+'''
+
+#Program-5-
+'''
+print("Hello")
+print("Python")
+'''
+#Program-6-
+'''
+print("MySirG")
+'''
+#Program-7-
+'''
+print("TeachersDay")
+'''
+#Program-8-
+'''
+a="uday singh arya"
+print(a)
+'''
+#Program-9-
+'''
+a="Uday Singh AryA"
+B=21
+Q="MCA"
+Y=2
+print("name=",a)
+print("age=",B)
+print("qualification=",Q)
+print("year=",Y)
+'''
+#Program-10-
+'''
+a=11
+b=22
+c=33
+print(a)
+print(b)
+print(c)
+'''
