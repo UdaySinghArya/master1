91cfbbdafd76664b48079cf49c22b59aba62c2fb
UdaySinghArya <118660131+UdaySinghArya@users.noreply.github.com>
2022-12-06 00:03:19 +0530

    Add files via upload

---
+++ assignment_10.py
@@ -0,0 +1,65 @@
+#Program-1-
+'''
+for x in range(1,11):
+    print(5*x)
+'''
+
+#Program-2-
+'''
+for x in range(1,11):
+    print(10*x)
+'''
+#Program-3-
+'''
+m=6
+for x in range(1,11):
+    print(6*x)
+'''
+#Program-4-
+'''
+for x in range(10,0,-1):
+    print(10*x)
+'''
+#Program-5-
+'''
+a=int(input("printr the table you want"))
+for x in range(1,11):
+    print(a*x)
+    a=a+1
+'''
+#Program-6-
+'''
+n=100
+for x in range(1,n+1):
+    if(x%2==0):
+        print(x)
+'''
+#Program-7-
+'''
+n=100
+for x in range(1,n+1):
+    if(x%2!=0):
+        print(x)
+'''
+#Program-8-
+'''
+n=5
+for x in range(1,n+1):
+    print(x*x)
+'''
+#Program-9-
+'''
+n=5
+for x in range(1,n+1):
+    print(x*x*x)
+'''
+#Program-10-
+'''
+for x in range(15,45):
+    if(x>1):
+        for y in range(2,x):
+            if(x%y==0):
+                break
+        else:
+            print(x)
+'''
---
+++ assignment_9.py
@@ -0,0 +1,87 @@
+#Prtogram-1-
+'''
+a=1
+n=10
+while(a<=n):
+    print("MySirG")
+    a=a+1
+'''
+#Program-2-
+'''
+a=1
+n=10
+while(a<=n):
+    print(a)
+    a=a+1
+'''
+#Program-3-
+'''
+a=10
+n=1
+while(a>=n):
+    print(a)
+    a=a-1
+'''
+#Program-4-
+'''
+a=1
+n=10
+while(a<=n):
+    if(a%2!=0):
+        print(a)
+    a=a+1
+'''
+
+
+#Program-5-
+'''
+a=10
+n=1
+while(a>=n):
+    if(a%2!=0):
+        print(a)
+    a=a-1
+'''
+#Program-6-
+'''
+a=1
+n=10
+while(a<=n):
+    if(a%2==0):
+        print(a)
+    a=a+1
+'''
+#Program-7-
+'''
+a=10
+n=1
+while(a>=n):
+    if(a%2==0):
+        print(a)
+    a=a-1
+'''
+#program-8-
+'''
+a=1
+n=10
+while(a<=n):
+    print(a*a)
+    a=a+1
+'''
+#Program-9-
+'''
+a=1
+n=10
+while(a<=n):
+    print(a*a*a)
+    a=a+1
+'''
+#Program-10-
+'''
+a=1
+n=10
+while(a<=n):
+    b=10*a
+    print("10*",a,"=",b)
+    a=a+1
+'''
