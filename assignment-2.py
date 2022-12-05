7e907ffc953c7634edc5d17ca98916fb18981213
UdaySinghArya <118660131+UdaySinghArya@users.noreply.github.com>
2022-12-06 00:05:54 +0530

    Add files via upload

---
+++ assignment-2.py
@@ -0,0 +1,58 @@
+#Program-1-
+'''
+print("Learning Python")   #comments
+'''
+#Program-2-
+'''Multiline comments write in  triple quotes'''
+'''
+a=11
+b=22
+c=33
+d=44
+print(a)
+print(b)
+print(c)
+print(d)
+'''
+#Program-3-
+'''
+a=35
+b="MySirG"
+c=5.46
+d=3+4j
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+'''
+
+#Program-4-
+'''
+a=11
+b=11
+print(id(a))
+print(id(b))
+'''
+#Program-5-
+'''
+a=11
+b=2.3
+c="uday"
+d=2+3j
+print("value=",a,"type=",type(a),"id=",id(a))
+print("value=",a,"type=",type(b),"id=",id(b))
+print("value=",a,"type=",type(c),"id=",id(c))
+print("value=",a,"type=",type(d),"id=",id(d))
+'''
+#program-7-
+'''
+import keyword
+print(keyword.kwlist)
+'''
+#Program-10-
+'''
+from datetime import datetime
+today=datetime.now()
+time_date=today.strftime("%d/%m/%Y %:%M:%S")
+print("date and time=",time_date)
+'''
